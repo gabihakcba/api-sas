768f1b7026e62698cf7102f33c5116d5f5b2dd22
Gabriel Hak <162186040+gabihakcba@users.noreply.github.com>
2025-10-13 16:01:50 -0300

    feat: protect cuenta endpoints with guards

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -2,12 +2,14 @@ import { Global, Module } from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { AuthController } from './auth.controller';
 import { CuentaModule } from '../cuenta/cuenta.module';
+import { AuthGuard } from './guards/auth.guard';
+import { RolesGuard } from './guards/roles.guard';
 
 @Global()
 @Module({
   imports: [CuentaModule],
-  providers: [AuthService],
+  providers: [AuthService, AuthGuard, RolesGuard],
   controllers: [AuthController],
-  exports: [AuthService],
+  exports: [AuthService, AuthGuard, RolesGuard],
 })
 export class AuthModule {}
